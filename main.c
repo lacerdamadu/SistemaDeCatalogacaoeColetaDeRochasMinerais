b77abbf5697f3f197803d7452891141d61111e28
Ana Clara <nanakkj.sillaby@gmail.com>
2024-11-12 13:35:39 -0300

    Melhorando teste

--- main.c
+++ main.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
+#include <string.h>
 #include "ListaMineirais.h"
 
 int main(){
     ListaMinerais ListaT;
     IniVListaM(&ListaT);
-    for(int i=0; i < 2; i++){
-        char nome[100];
-        scanf("%s", nome);
-        InsMineral(&ListaT, nome);
+
+    char input[300];    
+    char nome[100];
+
+ // Variável para armazenar temporariamente cada nome
+    int i, j = 0;
+
+    printf("Digite até três nomes separados por espaços:\n");
+    fgets(input, sizeof(input), stdin);
+
+    for (i = 0; i < strlen(input); i++) {
+        if (input[i] == ' ' || input[i] == '\n' || input[i] == '\0') {
+            nome[j] = '\0'; // Finaliza a string nome
+            if (strlen(nome) > 0) { // Verifica se nome não está vazio
+                InsMineral(&ListaT, nome); // Chama a função preenchemineral
+                j = 0; // Reseta o índice de nome para o próximo nome
+            }
+            if (input[i] == '\n' || input[i] == '\0') {
+                break; // Sai do loop se encontrar o fim da linha ou da string
+            }
+        } else {
+            nome[j++] = input[i];
+        }
     }
+
+
     ImprimeListaM(&ListaT);
 }
