c7c039bc2eb6a0f84da82a8cbfa31c2e9d34dad3
Raganzai101 <larag2077@gmail.com>
2024-11-16 20:29:02 -0300

    Commit ListaSondas

--- ListaSondas.c
+++ ListaSondas.c
@@ -1,62 +1,64 @@
 #include "ListaSondas.h"
 #include "Compartimento.h"
+#include "Sonda.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-void FazListaVazia(TSondas* sonda)
+void FazListaVazia(TSondas* Sondas)
 {
-     sonda->pPrimeiro = (Apontador) malloc(sizeof(TCelula_S));
-     sonda->pUltimo = sonda->pPrimeiro;
-     sonda->pPrimeiro->pProx = NULL;
+     Sondas->pPrimeiro = (Apontador) malloc(sizeof(TCelula_S));
+     Sondas->pUltimo = Sondas->pPrimeiro;
+     Sondas->pPrimeiro->pProx = NULL;
 }
 
 void Insere(TSondas* Sondas,TSonda* sonda){
      Sondas->pUltimo->pProx = (Apontador) malloc(sizeof(TCelula_S));
      Sondas->pUltimo = Sondas->pUltimo->pProx;
-     Sondas->pUltimo->Sonda = *sonda;
+     Sondas->pUltimo->Sondas = *sonda;
      Sondas->pUltimo->pProx = NULL;
 }
 
 int Retira(TSondas* Sondas, int retirar){
     TCelula_S* pAux;
-    pAux = Sondas->pPrimeiro;
-    TCelula_S* p_anterior = NULL;
+    pAux = Sondas->pPrimeiro->pProx;
+    TCelula_S* p_ant;
+    p_ant = Sondas->pPrimeiro;
 
     while (pAux != NULL){
-        if (pAux->Sonda.Identificador == retirar){
-            if (p_anterior == NULL){
+        if (pAux->Sondas.sonda.Identificador == retirar){
+            if (p_ant == NULL){
                 Sondas->pPrimeiro = pAux->pProx;
             } else{
-                p_anterior->pProx = pAux->pProx;
+                p_ant->pProx = pAux->pProx;
             }
             if (pAux->pProx == NULL){
-                Sondas->pUltimo = p_anterior;
+                Sondas->pUltimo = p_ant;
             }
             free(pAux);
             return 1;
         }
-        p_anterior = pAux;
+        p_ant = pAux;
         pAux = pAux->pProx;
     }
 }
 
 
-void Imprime(TSondas* Sonda, TSonda* sonda) {
+void Imprime(TSondas* Sondas, TSonda* sonda) {
     TCelula_S* pAux;
-    pAux = Sonda->pPrimeiro->pProx;
+    pAux = Sondas->pPrimeiro->pProx;
     if (pAux == NULL) {
         return;
     }
     while (pAux != NULL) {
-        printf("Identificador: %d\n", pAux->Sonda.Identificador);
-        printf("Latitude: %d\n", pAux->Sonda.Latitude);
-        printf("Longitude: %d\n", pAux->Sonda.Longitude);
-        printf("Peso: %d\n", pAux->Sonda.PesoMax);
-        printf("Velocidade: %d\n", pAux->Sonda.Velocidade);
-        printf("Ligado: %d\n", pAux->Sonda.EstaLigada);
-        printf("Combustivel: %d\n", pAux->Sonda.Combustivel);
-        printf("Compartimentos: %d\n", pAux->Sonda.compartimentos);
+        printf("Identificador: %d\n", pAux->Sondas.sonda.Identificador);
+        printf("Latitude: %d\n", pAux->Sondas.sonda.Latitude);
+        printf("Longitude: %d\n", pAux->Sondas.sonda.Longitude);
+        printf("Peso: %d\n", pAux->Sondas.sonda.PesoMax);
+        printf("Velocidade: %d\n", pAux->Sondas.sonda.Velocidade);
+        printf("Ligado: %d\n", pAux->Sondas.sonda.EstaLigada);
+        printf("Combustivel: %d\n", pAux->Sondas.sonda.Combustivel);
+        printf("Compartimentos: %d\n", pAux->Sondas.sonda.compartimentos);
         pAux = pAux->pProx;
     }
 }
