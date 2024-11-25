bdbb322e6c04e3d7a6e9a94a987fdf36e4193af9
Ana Clara <nanakkj.sillaby@gmail.com>
2024-11-25 00:17:26 -0300

    mudança

--- scr/Sistema de Controle/SistemaDeControle.h
+++ scr/Sistema de Controle/SistemaDeControle.h
@@ -9,10 +9,13 @@
 void Central(TSondas *lista);//Contem um menu interativo dando ao usuario a opção de decidir o que ele quer fazer
 int LeituraPorArquivo(TSondas *lista);//Lê o arquivo teste e passa os valores lidos para as outras funções
 int LeituraPeloTerminal(TSondas *lista);//Lê as informações digitadas no terminal e inicializa o que for passado
-void RedistribuiRochas(TSondas *ListaSondas, int numsondas);
-int VerificaSeTemQuemReceber(TSondas *lista, Compartimento *Comp, double peso);
-void OrdenaPesos(Sonda **Sondas, int numsondas);
-void PreencheVetor(TSondas *Sondas, Sonda **VetorSondas, int numsondas);
-void Coleta(Celula Rocha,TSondas *ListaSondas, int numsondas);
+void Coleta(Celula Rocha,TSondas *ListaSondas, int numsondas);//Escolhe a sonda mais próxima da rocha para inserir
+void RedistribuiRochas(TSondas *ListaSondas, int numsondas);//Faz a redistribuição de rochas
+
+
+//Funções auxiliares
+int VerificaSeTemQuemReceber(TSondas *lista, Compartimento *Comp, double peso);//
+void OrdenaPesos(Sonda **Sondas, int numsondas);//
+void PreencheVetor(TSondas *Sondas, Sonda **VetorSondas, int numsondas);//
 
 #endif
